104dea533c52bbec0b4677b6caf8f7c2d41e1624
Francis Ignacio <48373989+noeyignacio@users.noreply.github.com>
2021-03-11 13:11:10 +0800

    :star2: added inline event handlers

--- svelte-crash-course/src/App.svelte
+++ svelte-crash-course/src/App.svelte
@@ -1,62 +1,20 @@
 <script>
-	// const img = "favicon.png";
-	// var name = "Francis";
-	// var count = 0;
-
-	// /* Count Handlers */
-	// const countHandler = () => {
-	// 	count += 1;
-	// }
-	// const resetCountHandler = () => {
-	// 	count = 0;
-	// }
-
-	// /* Input Handlers */
-	// const inputHandler = (e) => {
-	// 	name = e.target.value;
-	// }
-	// const deleteInputHandler = () => {
-	// 	name = "";
-	// 	document.querySelector(".inputHandler").value = ""
-	// }
-
-	// var firstName = "";
-	// var lastName = "";
-
-	// /* Reactive Values */
-	// $: fullName = `${firstName} ${lastName}`
-	// const resetFullName = () => {
-	// 	fullName = ""
-	// 	document.querySelector(".firstName").value = "";
-	// 	document.querySelector(".lastName").value = "";
-	// }
-
-	const fruits = [
+	let fruits = [
 		{name: "Apple", color: "Red", quantity: "2", id: 1},
 		{name: "Orange", color: "Orange", quantity: "5", id: 2},
 		{name: "Banana", color: "Yellow", quantity: "8", id: 3},
 	]
+
+	const deleteFruit = (id) => {
+
+		/* If True: `fruit` will be kept. */
+		/* False: `fruit` will be remove. */
+		fruits = fruits.filter((fruit) => fruit.id != id) 
+		console.log(fruits)
+	}
 </script>
 
 <main>
-	<!-- <h1>Hi, I'm {name}!</h1>
-	<input on:input={inputHandler} class="inputHandler" type="text">
-	<button on:click={deleteInputHandler}>Reset Name</button>
-
-	<h3>Counter: {count}</h3>
-	<button on:click={countHandler}>Click</button>
-	<button on:click={resetCountHandler}>Reset Count</button> -->
-
-	<!-- <h1>Hello, I'm {fullName}!</h1>
-	
-	<label for="firstName">First Name:</label>
-	<input bind:value={firstName} class="firstName" type="text">
-	<label for="lastName">Last Name:</label>
-	<input bind:value={lastName} class="lastName" type="text">
-	<button on:click={resetFullName}>Reset Full Name</button>
-
-	<img src={img} alt="Svelte Logo"> -->
-
 	{#each fruits as fruit (fruit.id)}
 		<div class="ui cards">
 			<div class="card">
@@ -65,6 +23,7 @@
 					<div class="meta">Quantity: {fruit.quantity}</div>
 					<div class="description">Color: {fruit.color}</div>
 				</div>
+				<button class="ui red button" on:click={() => deleteFruit(fruit.id)}>Delete</button>
 			</div>
 		</div>
 	{:else}
