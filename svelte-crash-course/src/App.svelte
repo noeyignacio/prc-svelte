311d22c5429e0257fa5b5a65c9bcdf154fb5422c
Francis Ignacio <ignacionooey@prominence.co>
2021-03-11 09:53:44 +0800

    :star: added some basics of svelte

---
+++ svelte-crash-course/src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+	const img = "favicon.png";
+	var name = "";
+	var count = 0;
+
+	const countHandler = () => {
+		count += 1;
+	}
+
+	const inputHandler = (e) => {
+		name = e.target.value;
+	}
+</script>
+
+<main>
+	<h1>Hi, I'm {name}!</h1>
+	<input on:input={inputHandler} type="text">
+	<h3>Counter: {count}</h3>
+	<button on:click={countHandler}>Click</button>
+
+	<img src={img} alt="Svelte Logo">
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	h1 {
+		font-size: 72px;
+		text-align: center;
+	}
+</style>
