15e60bf42fc40b55e54d0a2b90b277742690b864
Francis Ignacio <48373989+noeyignacio@users.noreply.github.com>
2021-03-11 13:43:20 +0800

    :star2: added components

---
+++ svelte-crash-course/src/components/Announcement.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { fade } from 'svelte/transition'
+    let showAlret = true;
+
+    const hideAlret = () => {
+        showAlret = false;
+    };
+</script>
+
+{#if showAlret}
+    <div transition:fade class="ui message">
+        <i on:click={hideAlret} class="close icon"></i>
+        <div class="header">
+            50% off all fruits.
+        </div>
+        <div class="meta">Sign up for more discounts!!</div>
+    </div>
+{/if}
