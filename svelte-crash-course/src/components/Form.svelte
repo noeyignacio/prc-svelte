60d7d039d9d410346db3902be405fb6d3ce17159
Francis Ignacio <48373989+noeyignacio@users.noreply.github.com>
2021-03-11 15:21:55 +0800

    :star: added custom events

---
+++ svelte-crash-course/src/components/Form.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { v4 as uuidv4 } from 'uuid'; 
+    import { createEventDispatcher } from 'svelte'
+
+    let dispatch = createEventDispatcher()
+
+    let name, price;
+
+    const handleSubmit = () => {
+        const food = {
+            name: name,
+            price: price,
+            id: uuidv4()
+        }
+        dispatch("addFood", food);
+    }
+</script>
+
+<form on:submit|preventDefault={handleSubmit} class="ui form">
+    <div class="two fields">
+        <div class="field">
+            <label for="name">Food</label>
+            <input bind:value={name} type="text" placeholder="Name of Food">
+        </div>
+        <div class="field">
+            <label for="color">Price</label>
+            <input bind:value={price} type="text" placeholder="Price of Food">
+        </div>
+        <button class="ui right labeled icon button">
+            <i class="right arrow icon"></i>
+                Submit
+        </button>
+    </div>
+    
+</form>
