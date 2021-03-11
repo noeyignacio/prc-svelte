002c9318fec26bb83b67e468eef3d52c02b7777c
Francis Ignacio <ignacionooey@prominence.co>
2021-03-11 12:46:29 +0800

    :star: added loops and semantic ui.

--- svelte-crash-course/src/App.svelte
+++ svelte-crash-course/src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-	const img = "favicon.png";
+	// const img = "favicon.png";
 	// var name = "Francis";
 	// var count = 0;
 
@@ -20,16 +20,22 @@
 	// 	document.querySelector(".inputHandler").value = ""
 	// }
 
-	var firstName = "";
-	var lastName = "";
+	// var firstName = "";
+	// var lastName = "";
 
-	/* Reactive Values */
-	$: fullName = `${firstName} ${lastName}`
-	const resetFullName = () => {
-		fullName = ""
-		document.querySelector(".firstName").value = "";
-		document.querySelector(".lastName").value = "";
-	}
+	// /* Reactive Values */
+	// $: fullName = `${firstName} ${lastName}`
+	// const resetFullName = () => {
+	// 	fullName = ""
+	// 	document.querySelector(".firstName").value = "";
+	// 	document.querySelector(".lastName").value = "";
+	// }
+
+	const fruits = [
+		{name: "Apple", color: "Red", quantity: "2", id: 1},
+		{name: "Orange", color: "Orange", quantity: "5", id: 2},
+		{name: "Banana", color: "Yellow", quantity: "8", id: 3},
+	]
 </script>
 
 <main>
@@ -41,7 +47,7 @@
 	<button on:click={countHandler}>Click</button>
 	<button on:click={resetCountHandler}>Reset Count</button> -->
 
-	<h1>Hello, I'm {fullName}!</h1>
+	<!-- <h1>Hello, I'm {fullName}!</h1>
 	
 	<label for="firstName">First Name:</label>
 	<input bind:value={firstName} class="firstName" type="text">
@@ -49,17 +55,19 @@
 	<input bind:value={lastName} class="lastName" type="text">
 	<button on:click={resetFullName}>Reset Full Name</button>
 
-	<img src={img} alt="Svelte Logo">
-</main>
+	<img src={img} alt="Svelte Logo"> -->
 
-<style>
-	main {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-	}
-	h1 {
-		font-size: 72px;
-		text-align: center;
-	}
-</style>
+	{#each fruits as fruit (fruit.id)}
+		<div class="ui cards">
+			<div class="card">
+				<div class="content">
+					<div class="header">{fruit.name}</div>
+					<div class="meta">Quantity: {fruit.quantity}</div>
+					<div class="description">Color: {fruit.color}</div>
+				</div>
+			</div>
+		</div>
+	{:else}
+		<p>There is no Fruits.</p>
+	{/each}
+</main>
